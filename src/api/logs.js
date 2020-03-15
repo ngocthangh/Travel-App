05f1ba9cd34be4f64819736bc3351f19e1f1a0b1
thanghn2 <thanghn@antsprogrammatic.com>
2020-03-15 23:52:39 +0700

    init project - finish insert mongodb, load list locations inserted

---
+++ src/api/logs.js
@@ -0,0 +1,29 @@
+const { Router } = require('express')
+const LogEntry = require('../model/LogEntry')
+const router = Router()
+
+router.get('/', async (req, res, next) => {
+    try {
+        const entries = await LogEntry.find()
+        res.json(entries)
+        
+    } catch (error) {
+        next(error)
+    }
+})
+
+router.post('/', async (req, res, next) => {
+    try {
+        const logEntry = new LogEntry(req.body)
+        const createdEntry = await logEntry.save()
+        res.json(createdEntry)
+        console.log(req.body)
+    } catch (error) {
+        if(error.name === 'ValidationError') {
+            res.statusCode(422)
+        }
+        next(error)
+    }
+})
+
+module.exports = router
